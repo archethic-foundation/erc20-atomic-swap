25e1533d248f9cd6f4a5b1afc98f5be76da7999e
Neylix <julien.leclerc05@protonmail.com>
2023-03-24 14:24:28 +0100

    Add endTime control for withdraw in ERC contract

--- truffle/contracts/HTLC.sol
+++ truffle/contracts/HTLC.sol
@@ -26,8 +26,9 @@
 
    function withdraw(bytes32 _secret) public {
      require(finished == false, 'Swap already done');
-     require(sha256(abi.encodePacked(_secret)) == hash, 'wrong secret');
-     require(token.balanceOf(address(this)) > 0, 'not enough funds');
+     require(sha256(abi.encodePacked(_secret)) == hash, 'Wrong secret');
+     require(token.balanceOf(address(this)) > 0, 'Not enough funds');
+     require(block.timestamp < startTime + lockTime, 'Withdraw delay outdated, use refund');
      secret = _secret;
      token.transfer(recipient, amount);
      finished = true;
@@ -35,7 +36,7 @@
 
    function refund() public {
      require(finished == false, 'Swap already done');
-     require(block.timestamp > startTime + lockTime, 'too early');
+     require(block.timestamp >= startTime + lockTime, 'Too early');
      token.transfer(owner, amount);
      finished = true;
    }
